b7a65433dda46089755312288507ce7da7267a97
chenchengHZ <chencheng2080@sina.com>
2016-09-29 12:46:02 +0800

    创建工程

---
+++ MyApp/MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by 陈诚 on 16/9/29.
+//  Copyright © 2016年 陈诚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyApp/MyApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyApp
+//
+//  Created by 陈诚 on 16/9/29.
+//  Copyright © 2016年 陈诚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
